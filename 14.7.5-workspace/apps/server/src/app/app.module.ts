d4d472bc79d3cb2adbe0abcde56d175b42a37599
Zachary Kipping <zachary.kipping@oasisdigital.com>
2022-11-17 09:24:22 -0600

    Simplified example code in each repro

--- 14.7.5-workspace/apps/server/src/app/app.module.ts
+++ 14.7.5-workspace/apps/server/src/app/app.module.ts
@@ -1,10 +1,14 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { CrudModule } from './crud/crud.module';
+import * as jsonServer from 'json-server';
 
-@Module({
-  imports: [CrudModule],
-  controllers: [AppController],
-})
-export class AppModule {}
+@Module({})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    const router = jsonServer.router({
+      users: [{ id: '123asd', firstName: 'Foo', lastName: 'Bar', age: 33 }],
+    });
+    const middlewares = jsonServer.defaults({});
+    consumer.apply(...middlewares, router).forRoutes('/');
+  }
+}
